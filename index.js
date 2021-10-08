34355a7100efa0f922692008c28732cd0ea2c238
szolarisz <thomas@nyirszikszi.hu>
2021-10-08 18:22:11 +0200

    Az alap szerver elkészült

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const http = require('http');
 const fs = require('fs');
-const port = 4444
+const port = 4444;
+
+const server = http.createServer( (req,res) => {
+    console.log(req.url);
+    console.log(req.method);
+});
+
+server.listen( port);
