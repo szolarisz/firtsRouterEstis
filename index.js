4b7a2dd2197d686ccaccb531b5e0b49af9db9b8a
szolarisz <thomas@nyirszikszi.hu>
2021-10-08 19:24:59 +0200

    Kész alap router

--- index.js
+++ index.js
@@ -5,6 +5,44 @@ const port = 4444;
 const server = http.createServer( (req,res) => {
     console.log(req.url);
     console.log(req.method);
+
+    //router
+    switch(true) {
+        case req.url === "/" && req.method === "GET" :
+            fs.readFile('index.html', 'utf8', (err, data) => {
+                res.setHeader('Content-Type', 'text/html');
+                res.writeHead(200);
+                res.end(data);
+            })
+        break;
+        case req.url === "/alap.css" && req.method === "GET":
+
+            fs.readFile('alap.css', 'utf8', (err, data) => {
+                res.setHeader('Content-Type', 'text/css');
+                res.writeHead(200);
+                res.end(data);
+            })
+        break;
+        case req.url === "/alap2.css" && req.method === "GET":
+
+            fs.readFile('alap.css', 'utf8', (err, data) => {
+                res.setHeader('Content-Type', 'text/css');
+                res.writeHead(200);
+                res.end(data);
+            })
+        break;
+        default:
+            fs.readFile('hiba.html', 'utf8', (err, data) => {
+                res.setHeader('Content-Type', 'text/html');
+                res.writeHead(200);
+                res.end(data);
+            })
+
+    }
 });
 
 server.listen( port);
+
+/*
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+*/
